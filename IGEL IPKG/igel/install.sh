beda9b544afd5a6c47d1716f6140506ea1dcc16a
samstreets <116685391+samstreets@users.noreply.github.com>
2026-02-15 16:44:05 +0000

    Update install.sh

--- IGEL IPKG/igel/install.sh
+++ IGEL IPKG/igel/install.sh
@@ -3,9 +3,19 @@
 # Run containerd installation
 bash /services/greencloud/install-containerd.sh
 
-# Enable services
+# Enable services (but don't start them yet - config sync will handle that)
 enable_system_service containerd.service
 enable_system_service gcnode.service
 
 # Set executable permissions
 chmod +x /services/greencloud/usr/local/bin/*
+chmod +x /usr/local/bin/greencloud-register
+
+# Run config sync to create config files from IGEL parameters
+bash /services/greencloud/greencloud-config-sync.sh
+
+# Start containerd
+systemctl start containerd.service
+
+echo "GreenCloud installation complete"
+echo "Configure API key and node name in IGEL Setup, then run: greencloud-register"
